3f1ea7f93e431b01a247c91137b7c20cbd6258c4
Joel <ajiolajoel@gmail.com>
2023-10-13 14:32:41 +0100

    react-redux with api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import Header from './containers/Header';
+import { BrowserRouter as Router, Route, Navigate } from 'react-router-dom';
+import ProductList from './containers/ProductList';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-     <h3>hello</h3>
+      <Router>
+      <Header />
+      <Navigate>
+      <Route path='/' exact Component={ProductList}/>
+      <Route path='/product/:productId' exact Component={ProductDetail}/>
+      <Route>404 Not Found!</Route>
+      </Navigate>
+      </Router>
+   
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import { createStoreHook } from "react-redux";
+import { createStore } from "redux";
 import reducers from "./reducers/index";
 
-const store = createStoreHook(reducers, {},
+const store = createStore(reducers, {},
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
